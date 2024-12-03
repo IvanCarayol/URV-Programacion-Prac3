33ff75b207cc7e9289c4fd7f15287792ffe25abf
Waliguren <91369888+Waliguren@users.noreply.github.com>
2024-12-03 13:01:46 +0100

    Creacion clase Data y avance en la clase Membre

---
+++ practica_3/src/Data.java
@@ -0,0 +1,40 @@
+public class Data {
+    int dia, mes, any;
+
+    public Data(int dia, int mes, int any)
+    {
+        this.dia = dia;
+        this.mes = mes;
+        this.any = any;
+    }
+
+    public int getDia()
+    {
+        return dia;
+    }
+
+    public int getMes()
+    {
+        return mes;
+    }
+
+    public int getAny()
+    {
+        return any;
+    }
+
+    public void setDia(int dia)
+    {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes)
+    {
+        this.mes = mes;
+    }
+
+    public void setAny(int any)
+    {
+        this.any = any;
+    }
+}
--- practica_3/src/Membre.java
+++ practica_3/src/Membre.java
@@ -1,3 +1,63 @@
 public class Membre {
     
+String alias;
+Data dataAlta;
+String correu;
+Data dataBaixa;
+LlistaAssociacio associacions;
+
+public Membre (String alias, Data dataAlta, String correu, Data dataBaixa, LlistaAssociacio associacions)
+{
+    this.alias = alias;
+    this.dataAlta = dataAlta;
+    this.correu = correu;
+    this.dataBaixa = dataBaixa;
+    this.associacions = associacions;
+}
+
+public String getAlias()
+{
+    return alias;
+}
+
+public Data getDataAlta()
+{
+    return dataAlta;
+}
+
+public String getCorreu()
+{
+    return correu;
+}
+
+public Data getDataBaixa()
+{
+    return dataBaixa;
+}
+
+public LlistaAssociacio geLlistaAssociacions()
+{
+    return associacions;
+}
+
+public void setAlias(String alias)
+{
+    this.alias = alias;
+}
+
+public void setDataAlta(Data data)
+{
+    dataAlta = data;
+}
+
+public void setCorreu(String correu)
+{
+    this.correu = correu;
+}
+
+public void setLlistaAssociacion(LlistaAssociacio associacions)
+{
+    this.associacions = associacions;
+}
+
 }
