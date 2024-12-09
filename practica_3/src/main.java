5c308bea0eca8ddf22486d22128e075b0ef3513f
“Ivan <ivan.carayol@estudiants.urv.cat>
2024-12-09 09:19:51 +0100

    add .class in gitingore

--- practica_3/src/main.java
+++ practica_3/src/main.java
@@ -1,7 +1,15 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
 
-        
+        Data datIni = new Data(25, 11, 2023);
+
+
+        // Prueba de Membre
+        Membre ivan = new Membre("Ivan", datIni, null, null, null);
+
+        // pruebas de accio
+        Accio acccio1 = new Accio("Arquitectura", "Para todos", null, 0, null);
+
+        System.out.println(acccio1);
     }
 }
