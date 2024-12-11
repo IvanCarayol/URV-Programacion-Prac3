0080035551702e941ca0e7150c9f0db0cc17673d
Fortcraft <marcrozas05@gmail.com>
2024-12-11 18:22:31 +0100

    Preparacio de les fucnions 2.1 y 2.3

--- 17_Gestor-Associacions-ETSE/src/mainP.java
+++ 17_Gestor-Associacions-ETSE/src/mainP.java
@@ -1,6 +1,14 @@
+import java.io.File;
+
 public class mainP {
     public static void main(String[] args) {
+        // 2.1 Llistar associacions
+        File document
+        LlistaAssociacio lAssocicions = new LlistaAssociacio(100);
 
+        // 2.3. Llistar membres actius de cualsevol associacio
+        LlistaMembres lMembresActius = new LlistaMembres(100);
+        
         // 2.4. Llistar accions 
         LlistaAccio lAccio = new LlistaAccio(4);
         //lAccio.afegirAccio(null);
