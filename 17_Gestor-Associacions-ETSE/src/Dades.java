44f798805a38c07772deeccc0c36e87daa35927c
Waliguren <91369888+Waliguren@users.noreply.github.com>
2024-12-16 17:15:48 +0100

    Revert "Editada la clase dades"
    This reverts commit 904f0f82116292bd90e26769445d66e01417796c.

--- 17_Gestor-Associacions-ETSE/src/Dades.java
+++ 17_Gestor-Associacions-ETSE/src/Dades.java
@@ -1,24 +1,77 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.Reader;
 
 public class Dades 
 {
     private static String ruta = "data/";
-    ObjectOutputStream fitxer;
+    DataOutputStream fitxer;
     
     public void EscriureMembres(LlistaMembres membres)
     {
         try
         {
-            fitxer = new ObjectOutputStream(new FileOutputStream(ruta+"membres.dat"));
-            fitxer.writeObject(membres);
+            fitxer = new DataOutputStream(new FileOutputStream(ruta+"membres.dat"));
+
+            for (int i = 0; i < membres.getNumelem(); i++)
+            {
+                Membre membre = membres.getMembreAt(i);
+                if (membre.getClass().getName() == "Membre")
+                {
+                    fitxer.writeInt(0);
+                    escriureNomiCorreu(fitxer, membre);
+                    
+                }
+            }
 
         }
         catch (IOException e)
         {
-            System.out.println("Error en l'arxiu de sortida.");
+
         }
     }
 
+    private void escriureNomiCorreu(DataOutputStream fitxer ,Membre membre)
+    {
+        try
+        {
+            fitxer.writeUTF(membre.getAlias());
+            fitxer.writeUTF(membre.getCorreu());
+        } 
+        catch (Exception e) 
+        {
+
+        }
+        
+    }
+
+    private void escriureAssociacions(DataOutputStream fitxer, Membre membre)
+    {
+        try
+        {
+            int numAssociacions = membre.getLlistaAssociacions().getNumelem();
+
+            if (numAssociacions != 0)
+            {
+                for (int i = 0; i < numAssociacions; i++)
+                {
+                    Associacio associacio = membre.getLlistaAssociacions().getAsociacioAt(i);
+                    fitxer.writeInt(membre.getDataAlta(i).getDia());
+                    fitxer.writeInt(membre.getDataAlta(i).getMes());
+                    fitxer.writeInt(membre.getDataAlta(i).getAny());
+                    fitxer.writeUTF(associacio.getNom());
+                    fitxer.writeInt(membre.getDataBaixa(i).getDia());
+                    fitxer.writeInt(membre.getDataBaixa(i).getMes());
+                    fitxer.writeInt(membre.getDataBaixa(i).getAny());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
 }
