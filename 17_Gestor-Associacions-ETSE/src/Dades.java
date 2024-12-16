276379a64fae87266e775a43f552a2737f509478
Waliguren <91369888+Waliguren@users.noreply.github.com>
2024-12-16 19:07:40 +0100

    Cargar miembros de un archivo en proceso

--- 17_Gestor-Associacions-ETSE/src/Dades.java
+++ 17_Gestor-Associacions-ETSE/src/Dades.java
@@ -1,77 +1,119 @@
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
 
 public class Dades 
 {
     private static String ruta = "data/";
-    DataOutputStream fitxer;
     
     public void EscriureMembres(LlistaMembres membres)
     {
         try
         {
-            fitxer = new DataOutputStream(new FileOutputStream(ruta+"membres.dat"));
-
+            BufferedWriter fitxerOut = new BufferedWriter(new FileWriter(ruta+"membres.txt"));
+            String buffer;
+            
             for (int i = 0; i < membres.getNumelem(); i++)
             {
                 Membre membre = membres.getMembreAt(i);
-                if (membre.getClass().getName() == "Membre")
+
+                if (membre instanceof Professor)
                 {
-                    fitxer.writeInt(0);
-                    escriureNomiCorreu(fitxer, membre);
+                    buffer = "1;";
+                    buffer += escriureNomiCorreu(membre);
+                    buffer += ((Professor)membre).getNomDepartament()+";";
+                    buffer += ((Professor)membre).getNumeroDespatx()+";";
+                    buffer += escriureAssociacions(membre);
                     
                 }
-            }
+                else if (membre instanceof Alumne)
+                {
+                    buffer = "2;";
+                    buffer += escriureNomiCorreu(membre);
+                    buffer += ((Alumne)membre).getTitulacio()+";";
+                    buffer += ((Alumne)membre).isGraduat() ? "1;" : "0;";
+                    buffer += escriureAssociacions(membre);
 
+                }
+                else 
+                {
+                    buffer = "0;";
+                    buffer += escriureNomiCorreu(membre);
+                    buffer += escriureAssociacions(membre);
+                }
+                fitxerOut.write(buffer);
+                fitxerOut.newLine();
+            }
+            fitxerOut.close();
+
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println("L'arxiu d'entrada no existeix");
         }
         catch (IOException e)
         {
-
+            System.out.println("S'ha produit un error en els arxius");
         }
     }
 
-    private void escriureNomiCorreu(DataOutputStream fitxer ,Membre membre)
+    private String escriureNomiCorreu(Membre membre)
     {
-        try
-        {
-            fitxer.writeUTF(membre.getAlias());
-            fitxer.writeUTF(membre.getCorreu());
-        } 
-        catch (Exception e) 
-        {
+        String buffer;
 
-        }
+        buffer = membre.getAlias()+";";
+        buffer += membre.getCorreu()+";";
+
+        return buffer;
         
     }
 
-    private void escriureAssociacions(DataOutputStream fitxer, Membre membre)
+    private String escriureAssociacions(Membre membre)
     {
-        try
-        {
-            int numAssociacions = membre.getLlistaAssociacions().getNumelem();
+        String buffer = "";
+        int numAssociacions = membre.getLlistaAssociacions().getNumelem();
 
-            if (numAssociacions != 0)
+        if (numAssociacions != 0)
+        {
+            for (int i = 0; i < numAssociacions; i++)
             {
-                for (int i = 0; i < numAssociacions; i++)
-                {
-                    Associacio associacio = membre.getLlistaAssociacions().getAsociacioAt(i);
-                    fitxer.writeInt(membre.getDataAlta(i).getDia());
-                    fitxer.writeInt(membre.getDataAlta(i).getMes());
-                    fitxer.writeInt(membre.getDataAlta(i).getAny());
-                    fitxer.writeUTF(associacio.getNom());
-                    fitxer.writeInt(membre.getDataBaixa(i).getDia());
-                    fitxer.writeInt(membre.getDataBaixa(i).getMes());
-                    fitxer.writeInt(membre.getDataBaixa(i).getAny());
-                }
-            }
-        }
-        catch (Exception e)
-        {
+                Associacio associacio = membre.getLlistaAssociacions().getAsociacioAt(i);
 
+                buffer += membre.getDataAlta(i).getDia()+"/";
+                buffer += membre.getDataAlta(i).getMes()+"/";
+                buffer += membre.getDataAlta(i).getAny()+";";
+                buffer += associacio.getNom()+";";
+                buffer += membre.getDataBaixa(i).getDia()+"/";
+                buffer += membre.getDataBaixa(i).getMes()+"/";
+                buffer += membre.getDataBaixa(i).getAny()+";";
+            }
+
+            return buffer;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    public void LlegirMembres()
+    {
+        try 
+        {
+            BufferedReader fitxerIn = new BufferedReader(new FileReader(ruta+"membres.txt"));
+            
+            String linia;
+            linia = fitxerIn.readLine();
+
+            
+
+        } 
+        catch (Exception e) 
+        {
+            // TODO: handle exception
         }
     }
 }
