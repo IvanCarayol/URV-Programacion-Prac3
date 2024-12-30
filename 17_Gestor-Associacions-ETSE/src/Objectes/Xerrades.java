a1b44cfd7fa89697f943e9f0831ec1c5cb378a33
“Ivan <ivan.carayol@estudiants.urv.cat>
2024-12-30 10:26:26 +0100

    update atributos xerrades

--- 17_Gestor-Associacions-ETSE/src/Objectes/Xerrades.java
+++ 17_Gestor-Associacions-ETSE/src/Objectes/Xerrades.java
@@ -3,7 +3,8 @@ public class Xerrades extends Accio {
 
     private LlistaMembres lMembres;      
     private LlistaValoracio lValoracions; 
-    private int nElem;
+    private int nMem;
+    private int nValo;
     private int nElemMAX;
 
     // Constructor
@@ -14,7 +15,8 @@ public class Xerrades extends Accio {
         lMembres = new LlistaMembres(n_max);
         lValoracions = new LlistaValoracio(n_max, nom);
         nElemMAX = n_max;
-        nElem = 0;
+        nMem = 0;
+        nValo = 0;
     }
 
     public int numAssisten() {
@@ -35,10 +37,11 @@ public class Xerrades extends Accio {
      * @param valoracio de tipus Valoracio
      */
     public void afegirMembreValoracio(Valoracio valoracio) {
-        if(nElem < nElemMAX) {
+        if(nValo < nElemMAX && nMem < nElemMAX) {
             lMembres.afegirMembre(valoracio.getMembre());
             lValoracions.afegirValoracio(valoracio);
-            nElem++;
+            nValo++;
+            nMem++;
         }
     }
 
@@ -47,11 +50,21 @@ public class Xerrades extends Accio {
      * @param valoracio La valoración a añadir. 
     */ 
     public void valorarXerrada(Valoracio valoracio) {
-        if (valoracio != null && valoracio.valoracioValida(valoracio.getValoracio())){ 
-            lValoracions.afegirValoracio(valoracio); 
-        } else { 
-            throw new IllegalArgumentException("La valoración no es válida."); 
-        } 
+        if(nValo < nElemMAX) {
+            if (valoracio != null && valoracio.valoracioValida(valoracio.getValoracio())){ 
+                lValoracions.afegirValoracio(valoracio); 
+                nValo++;
+            } else { 
+                throw new IllegalArgumentException("La valoración no es válida."); 
+            } 
+        }
+    }
+
+    public void afegirMembre(Membre membre) {
+        if(nValo < nElemMAX) {
+            lMembres.afegirMembre(membre);
+            nMem++;
+        }
     }
 
     @Override
